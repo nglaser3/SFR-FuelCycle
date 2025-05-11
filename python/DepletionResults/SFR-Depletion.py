54d887a9880629a06ffa3b60f842f9228192f0d5
Nathan Glaser <nglaser3@illinois.edu>
2025-05-10 20:34:41 -0500

    OMG WORK PLEASE PLEASEPLEASE

--- python/DepletionResults/SFR-Depletion.py
+++ python/DepletionResults/SFR-Depletion.py
@@ -10,6 +10,6 @@ power = 286e6
 life_cycle = 20 * 365 * 24 * 3600
 times = [np.float64(3600)]*3 + list(np.diff(np.geomspace(3600*3, life_cycle, num = 18)))
 
-operator = openmc.deplete.CoupledOperator(sfr_model, 'chain_endfb71_sfr.xml')
+operator = openmc.deplete.CoupledOperator(sfr_model, '../chain_endfb71_sfr.xml')
 integrator = openmc.deplete.CECMIntegrator(operator=operator, timesteps=times, power=power, timestep_units='s')
 integrator.integrate()

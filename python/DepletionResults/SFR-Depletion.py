249783b7ac403a2186334d0823b4d8a7e08e378d
Nathan Glaser <nglaser3@illinois.edu>
2025-05-10 20:28:51 -0500

    Decrease particles and batches, convert to CECM

--- python/DepletionResults/SFR-Depletion.py
+++ python/DepletionResults/SFR-Depletion.py
@@ -11,5 +11,5 @@ life_cycle = 20 * 365 * 24 * 3600
 times = [np.float64(3600)]*3 + list(np.diff(np.geomspace(3600*3, life_cycle, num = 18)))
 
 operator = openmc.deplete.CoupledOperator(sfr_model, 'chain_endfb71_sfr.xml')
-integrator = openmc.deplete.LEQIIntegrator(operator=operator, timesteps=times, power=power, timestep_units='s')
+integrator = openmc.deplete.CECMIntegrator(operator=operator, timesteps=times, power=power, timestep_units='s')
 integrator.integrate()

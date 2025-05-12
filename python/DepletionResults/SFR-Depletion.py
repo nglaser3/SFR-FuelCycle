0a55b2ea9f56029780b0a3a7b3533a65eb2d6a72
Nathan Glaser <nglaser3@ajnovak2-serv-01.npre.illinois.edu>
2025-05-12 16:04:29 -0500

    Delete trash results

--- python/DepletionResults/SFR-Depletion.py
+++ python/DepletionResults/SFR-Depletion.py
@@ -10,6 +10,6 @@ power = 286e6
 times = [0.25, .75, 1., 5., 10., 30., 60., 365., 1353., 1825., 1825., 1825., 3650., 3650., 3650., 3650.]
 
 operator = openmc.deplete.CoupledOperator(sfr_model, '../Model/chain_endfb71_sfr.xml')
-operator.output_dir = '10kp_50i_100b'
+operator.output_dir = '20kp_50i_150b'
 integrator = openmc.deplete.CECMIntegrator(operator=operator, timesteps=times, power=power, timestep_units='d')
 integrator.integrate()

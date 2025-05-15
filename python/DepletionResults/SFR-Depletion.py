a61a01a03f6c61d831499e693fe64ec91af76c72
Nathan Glaser <nglaser3@ajnovak2-serv-01.npre.illinois.edu>
2025-05-15 01:10:39 -0500

    Doing decay analysis

--- python/DepletionResults/SFR-Depletion.py
+++ python/DepletionResults/SFR-Depletion.py
@@ -6,10 +6,10 @@ os.environ['OMP_NUM_THREADS'] = '40'
 
 sfr_model = openmc.Model.from_model_xml('../Model/sfr_model.xml')
 
-power = 286e6
-times = [0.25, .75, 1., 5., 10., 30., 60., 365., 1353., 1825., 1825., 1825., 3650., 3650., 3650., 3650.]
+power = [286e6]*12 + [0]*20 
+times = [0.25, .75, 1., 5., 10., 30., 60., 365., 1353., 1825., 1825., 1825.] + [10*365]*10 + [9900*365] + [10000*365]*9
 
 operator = openmc.deplete.CoupledOperator(sfr_model, '../Model/chain_endfb71_sfr.xml')
-operator.output_dir = '20kp_50i_150b'
+operator.output_dir = 'activity_at_'
 integrator = openmc.deplete.CECMIntegrator(operator=operator, timesteps=times, power=power, timestep_units='d')
 integrator.integrate()
